9dba35bfdd131f375846e2c2c4961e69d4c2afa2
Assaad Howayek <ahowa022@uottawa.ca>
2022-03-20 18:14:47 -0400

    Updates

---
+++ precache-manifest.43ed1789fd09142b47c3dcac18a66b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08005d160e9604aebd3bfc031dd5d91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40acf32d5d826006aa12",
+    "url": "/static/css/main.b0b12312.chunk.css"
+  },
+  {
+    "revision": "07b60bd347e50dfcfac3",
+    "url": "/static/js/2.2a55f751.chunk.js"
+  },
+  {
+    "revision": "eed60a401b496f512099fae73e059baa",
+    "url": "/static/js/2.2a55f751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40acf32d5d826006aa12",
+    "url": "/static/js/main.f22d227d.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
